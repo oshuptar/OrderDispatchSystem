112879344a44d622810dd1920730f84087a302b3
Oleh Shuptar <01193038@pw.edu.pl>
2026-02-08 08:23:23 +0100

    refactor: userService - added atomicity to operation of registering new user

--- UserService/UserService.Application/Mediator/CommandHandlers/UserRegisterCommandHandler.cs
+++ UserService/UserService.Application/Mediator/CommandHandlers/UserRegisterCommandHandler.cs
@@ -16,34 +16,51 @@ public class UserRegisterCommandHandler(UserManager<User> userManager,
                                         IUserProfileRepository userProfileRepository
     ) : ICommandHandler<UserRegisterRequestDto, UserRegisterResponseDto>
 {
-    
-    public async Task<UserRegisterResponseDto> HandleCommandAsync(UserRegisterRequestDto command, CancellationToken cancellationToken)
+
+    // Creating user and profile is atomic
+    public async Task<UserRegisterResponseDto> HandleCommandAsync(UserRegisterRequestDto command,
+        CancellationToken cancellationToken)
     {
-        logger.LogInformation("Registering user {Email}",  command.Email);
+        logger.LogInformation("Registering user {Email}", command.Email);
         var transaction = await unitOfWork.BeginTransactionAsync(cancellationToken);
-        User user = UserMapper.ToEntity(command);
-        UserProfile profile = UserProfileMapper.ToEntity(command);
-        var result = await userManager.CreateAsync(user, command.Password);
-        if (!result.Succeeded)
+        try
         {
-            await unitOfWork.RollbackTransactionAsync(transaction, cancellationToken);
-            String errors = String.Join(separator: ", ", result.Errors.Select(e => e.Description));
-            logger.LogError(errors);
-            
-            // Do I have to map errors to custom exceptions here?
-            if(result.Errors.Any(error => 
-                   error.Code == nameof(IdentityErrorDescriber.ConcurrencyFailure) 
-                   || error.Code == nameof(IdentityErrorDescriber.DefaultError)))
-                throw new Exception("An error occured while registering user");
-            
-            throw new  Exception($"An error occured while registering user: ${errors}");
+            User user = UserMapper.ToEntity(command);
+            UserProfile profile = UserProfileMapper.ToEntity(command);
+            var result = await userManager.CreateAsync(user, command.Password);
+            if (!result.Succeeded)
+            {
+                await unitOfWork.RollbackTransactionAsync(transaction, cancellationToken);
+                String errors = String.Join(separator: ", ", result.Errors.Select(e => e.Description));
+                logger.LogError(errors);
+
+                // Do I have to map errors to custom exceptions here?
+                if (result.Errors.Any(error =>
+                        error.Code == nameof(IdentityErrorDescriber.ConcurrencyFailure)
+                        || error.Code == nameof(IdentityErrorDescriber.DefaultError)))
+                    throw new Exception("An error occured while registering user");
+
+                throw new Exception($"An error occured while registering user: ${errors}");
+            }
+            profile.UserId = user.Id;
+            user.UserProfile = profile;
+            await userProfileRepository.CreateUserProfileAsync(profile, cancellationToken);
+            await unitOfWork.SaveChangesAsync(cancellationToken);
+            await unitOfWork.CommitTransactionAsync(transaction, cancellationToken);
+            logger.LogInformation("Registering user {Email} succeeded", command.Email);
+            return user.ToResponse();
         }
-        profile.UserId = user.Id;
-        user.UserProfile = profile;
-        await userProfileRepository.CreateUserProfileAsync(profile, cancellationToken);
-        await unitOfWork.SaveChangesAsync(cancellationToken);
-        await unitOfWork.CommitTransactionAsync(transaction, cancellationToken);
-        logger.LogInformation("Registering user {Email} - Success", command.Email);
-        return user.ToResponse();
+        catch (OperationCanceledException)
+        {
+            logger.LogInformation("Registering user {Email} cancelled", command.Email);
+            await unitOfWork.RollbackTransactionAsync(transaction, CancellationToken.None);
+            throw;
+        }
+        catch (Exception ex)
+        {
+            logger.LogError("Registering user {Email} failed: {message}", command.Email, ex.Message);
+            await unitOfWork.RollbackTransactionAsync(transaction, cancellationToken);
+            throw;
+        }
     }
 }
